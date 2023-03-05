99ce087b26bbfb0d8ac4d025fc6d7d55db6ed672
LydianJay <63202126+LydianJay@users.noreply.github.com>
2023-03-05 14:28:09 +0800

    Added Some classes
    -Added class for admin
    -Added class for client
    -Added class for book 
    -Added 3 different package

---
+++ LMS/src/book/Book.java
@@ -0,0 +1,57 @@
+package book;
+
+public class Book {
+	
+	
+	
+	private String bookName, bookAuthor;
+	private int bookYear;
+	private int bookCatID;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getBookAuthor() {
+		return bookAuthor;
+	}
+	public void setBookAuthor(String bookAuthor) {
+		this.bookAuthor = bookAuthor;
+	}
+	public int getBookYear() {
+		return bookYear;
+	}
+	public void setBookYear(int bookYear) {
+		this.bookYear = bookYear;
+	}
+	public int getBookCatID() {
+		return bookCatID;
+	}
+	public void setBookCatID(int bookCatID) {
+		this.bookCatID = bookCatID;
+	}
+	
+	
+	
+	
+}
---
+++ LMS/src/user/Admin.java
@@ -0,0 +1,49 @@
+package user;
+
+public class Admin {
+	private int adminID;
+	private String adminName;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public int getAdminID() {
+		return adminID;
+	}
+	public void setAdminID(int adminID) {
+		this.adminID = adminID;
+	}
+	public String getAdminName() {
+		return adminName;
+	}
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
