fdda74b89f6221826547c2162562cc7754685783
LydianJay <63202126+LydianJay@users.noreply.github.com>
2023-03-05 13:50:15 +0800

    Additionional Templates

--- LMS/src/main/Main.java
+++ LMS/src/main/Main.java
@@ -18,6 +18,20 @@ import javax.swing.border.LineBorder;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
+import java.awt.CardLayout;
+import javax.swing.JInternalFrame;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JTabbedPane;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.SpringLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.EtchedBorder;
 
 public class Main extends JFrame {
 
@@ -47,21 +61,20 @@ public class Main extends JFrame {
 		setTitle("Library Management System");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 685, 461);
+		setBounds(100, 100, 793, 537);
 		mainPanel = new JPanel();
 		mainPanel.setBackground(new Color(0, 128, 0));
 		mainPanel.setForeground(SystemColor.text);
 		mainPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(mainPanel);
-		mainPanel.setLayout(null);
+		mainPanel.setLayout(new CardLayout(0, 0));
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel.setBounds(142, 138, 405, 257);
 		panel.setBackground(new Color(128, 255, 128));
 		panel.setForeground(new Color(128, 255, 0));
-		mainPanel.add(panel);
+		mainPanel.add(panel, "name_4327871187200");
 		panel.setLayout(null);
 		
 		JLabel lbl_Login = new JLabel("Login");
@@ -95,5 +108,47 @@ public class Main extends JFrame {
 		JButton btnNewButton = new JButton("Login");
 		btnNewButton.setBounds(162, 194, 89, 23);
 		panel.add(btnNewButton);
+		
+		JPanel HomePagepanel = new JPanel();
+		HomePagepanel.setBorder(new EtchedBorder(EtchedBorder.RAISED, null, null));
+		mainPanel.add(HomePagepanel, "name_8739715910499");
+		HomePagepanel.setLayout(null);
+		
+		JPanel sidePanel = new JPanel();
+		sidePanel.setBackground(Color.GRAY);
+		sidePanel.setBounds(0, 0, 114, 488);
+		HomePagepanel.add(sidePanel);
+		
+		JButton selection1 = new JButton("Rent");
+		selection1.setFont(new Font("Tahoma", Font.BOLD, 18));
+		selection1.setForeground(Color.BLACK);
+		sidePanel.setLayout(new GridLayout(0, 1, 0, 0));
+		sidePanel.add(selection1);
+		
+		JButton selection2 = new JButton("Search");
+		selection2.setFont(new Font("Tahoma", Font.BOLD, 16));
+		sidePanel.add(selection2);
+		
+		JButton selection3 = new JButton("New button");
+		sidePanel.add(selection3);
+		
+		JButton selection4 = new JButton("New button");
+		sidePanel.add(selection4);
+		
+		JPanel homeDisplayPanel = new JPanel();
+		homeDisplayPanel.setBackground(Color.LIGHT_GRAY);
+		homeDisplayPanel.setBounds(114, 0, 653, 488);
+		HomePagepanel.add(homeDisplayPanel);
+		homeDisplayPanel.setLayout(new CardLayout(0, 0));
+		
+		JPanel queryPanel = new JPanel();
+		queryPanel.setBackground(Color.ORANGE);
+		homeDisplayPanel.add(queryPanel, "name_8929418439700");
+		queryPanel.setLayout(null);
+		
+		JPanel searchPanel = new JPanel();
+		searchPanel.setBackground(Color.CYAN);
+		homeDisplayPanel.add(searchPanel, "name_8933922325100");
+		searchPanel.setLayout(null);
 	}
 }
